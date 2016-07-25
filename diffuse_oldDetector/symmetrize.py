344018377eed8de3cbed9d3b1ab6415198642f7c
Tyler Chase <tylerchase56@gmail.com>
2016-07-25 11:58:11 -0700

    added option for accounting for expansion of the crystal as function of time

--- diffuse_oldDetector/symmetrize.py
+++ diffuse_oldDetector/symmetrize.py
@@ -23,6 +23,8 @@ import Region_Of_Interest_2
 import Fit_Peaks_2
 from scipy.ndimage.interpolation import shift
 from scipy.ndimage.interpolation import rotate
+from expand_contract import peak_expansion_f
+from expand_contract import expand
 
 
 
@@ -33,14 +35,16 @@ from scipy.ndimage.interpolation import rotate
 pixel_accuracy = 100      # Align with accuracy of 1/(pixel_accuracy)
 ROI_width = 64      # Make sure this is a power of 2
 # Addresses for loading and saving
-load_address = r'C:\Users\tchase56\Documents\UED\Ni\data\20160628\long\\'
-save_address = r'C:\Users\tchase56\Documents\UED\Ni\data\20160628\long\\'
+load_address = r'C:\Users\tchase56\Documents\UED\Gold_Diffuse_03212016\20160512\short_scan\\'
+save_address = r'C:\Users\tchase56\Documents\UED\Gold_Diffuse_03212016\20160512\short_scan\\'
 # Is this the output of align_average or the output of align_different_scans
 different_scans = 1
 # Number of innermost peaks that you would like to align with respect to 
 nbpeaks = 4
 # What fold of rotation symmetry is the pattern (ex. 4-fold symmetry for FCC)
 fold = 4
+# Flag to account for peak expansion in diffraction pattern (adds expansion interpolation)
+expansion_interpolation = 1
 
 
 
@@ -71,25 +75,45 @@ runs = []
 for i in range(0, np.shape(temp)[0]):
     runs.append(temp[i,:,:])
 
-# Center the image (using the average postion of the six innermost peaks)
-[peak_region, background_region] = Region_Of_Interest_2.GetRegionOfInterest(runs[0] ,nbpeaks, halfLength=(ROI_width/2), contrastFactor = 0.25)
+# Center the image (using the average postion of the nbpeaks innermost peaks)
+[peak_region, background_region] = Region_Of_Interest_2.GetRegionOfInterest(runs[0] ,nbpeaks, 
+    halfLength=(ROI_width/2), contrastFactor = 0.25, message = 'Select Innermost Peaks for centering')
 image_center = np.shape(temp[0,:,:])[0]/2 + 0.5 -1
 diffraction_shift = []
 diffraction_center = []
 for i in range(0,np.shape(runs)[0]):
     [peak_fit_pump, peak_intensity_pump, background_intensity] = Fit_Peaks_2.FitPeaks(runs[i], peak_region, background_region, halfLength=ROI_width/2)
-    #y_diffraction = np.mean([peak_fit_pump[0][0][1], peak_fit_pump[1][0][1], peak_fit_pump[2][0][1], peak_fit_pump[3][0][1]])
-    #x_diffraction = np.mean([peak_fit_pump[0][1][1], peak_fit_pump[1][1][1], peak_fit_pump[2][1][1], peak_fit_pump[3][1][1]])
     y_diffraction = np.mean([peak_fit_pump[l][0][1] for l in range(nbpeaks)])
     x_diffraction = np.mean([peak_fit_pump[l][1][1] for l in range(nbpeaks)])
     diffraction_shift.append([(image_center-x_diffraction),(image_center-y_diffraction)])
     diffraction_center.append([x_diffraction,y_diffraction])
 runs_centered = []    
 for i in range(0,np.shape(runs)[0]):
-    runs_centered.append(shift(runs[i],diffraction_shift[i]))    
+    runs_centered.append(shift(runs[i],diffraction_shift[i])) 
+if expansion_interpolation == 1:
+    [peak_region, background_region] = Region_Of_Interest_2.GetRegionOfInterest(runs[0] ,nbpeaks, 
+        halfLength=(ROI_width/2), contrastFactor = 0.25, message = 'Select Innermost Peaks for expansion interpolation')
+    [peak_fit_pump, peak_intensity_pump, background_intensity] = Fit_Peaks_2.FitPeaks(runs_centered[0], peak_region, background_region, halfLength=ROI_width/2)
+    peak_distance_ref = peak_expansion_f(np.array(peak_fit_pump)[:,0,1], np.array(peak_fit_pump)[:,1,1])
+    peak_distance = []
+    peak_distance.append(peak_distance_ref)
+    peak_distance_expanded = []
+    peak_distance_expanded.append(peak_distance_ref)
+    runs_expanded = []
+    runs_expanded.append(runs_centered[0])
+    for i in range(1,np.shape(runs)[0]):
+        [peak_fit_pump, peak_intensity_pump, background_intensity] = Fit_Peaks_2.FitPeaks(runs_centered[i], peak_region, background_region, halfLength=ROI_width/2)
+        peak_distance.append(peak_expansion_f(np.array(peak_fit_pump)[:,0,1], np.array(peak_fit_pump)[:,1,1]))
+        factor = peak_distance[0]/peak_distance[i]
+        expanded = expand(runs_centered[i],factor)
+        runs_expanded.append(expanded)
+        [peak_fit_pump, peak_intensity_pump, background_intensity] = Fit_Peaks_2.FitPeaks(expanded, peak_region, background_region, halfLength=ROI_width/2)
+        peak_distance_expanded.append(peak_expansion_f(np.array(peak_fit_pump)[:,0,1], np.array(peak_fit_pump)[:,1,1]))
+    runs_centered = runs_expanded #line added to not break following code after adding expansion interpolation
 
 # Rotate the image
-[peak_region, background_region] = Region_Of_Interest_2.GetRegionOfInterest(runs_centered[0] ,2, halfLength=(ROI_width/2), contrastFactor = 0.25)
+[peak_region, background_region] = Region_Of_Interest_2.GetRegionOfInterest(runs_centered[0] ,2, 
+    halfLength=(ROI_width/2), contrastFactor = 0.25, message = 'Select 2 peaks that you would like to lie flat in rotated image')
 [peak_fit_pump, peak_intensity_pump, background_intensity] = Fit_Peaks_2.FitPeaks(runs_centered[0], peak_region, background_region, halfLength=ROI_width/2)
 deltaY = peak_fit_pump[1][1][1] - peak_fit_pump[0][1][1]
 deltaX = peak_fit_pump[1][0][1] - peak_fit_pump[0][0][1]
@@ -97,7 +121,7 @@ angle = (360/(2*np.pi))*np.arctan(deltaY/deltaX)
 rotated = []
 for i in range(0,np.shape(runs)[0]):
     rotated.append(rotate(runs_centered[i], angle))
-    
+       
 # Symmetrize image
 symmetric = []
 for i in range(np.shape(rotated)[0]):
@@ -109,5 +133,17 @@ for i in range(np.shape(symmetric)[0]):
     plt.imshow(symmetric[i])
     plt.show()
  
-# Save symmetrized images   
-np.save(save_address + 'symmetrized.npy', symmetric)
+if expansion_interpolation == 1:
+    # Save symmetrized images   
+    np.save(save_address + 'expanded_symmetrized.npy', symmetric)
+    np.save(save_address + 'expanded.npy', rotated)
+    plt.figure()
+    plt.title('Expansion Interpolation Test')
+    plt.plot(peak_distance, label = 'original')
+    plt.plot(peak_distance_expanded, label = 'expanded')
+    plt.legend()
+    plt.show()
+else:
+    # Save symmetrized images   
+    np.save(save_address + 'symmetrized.npy', symmetric)
+    np.save(save_address + 'centered_rotated.npy', rotated)
