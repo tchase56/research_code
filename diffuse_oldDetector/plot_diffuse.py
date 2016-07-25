5ed5023af2550a42ce56e69f1862d8effaea4bb9
Tyler Chase <tylerchase56@gmail.com>
2016-07-25 11:56:53 -0700

    can take in expanded images as well, additional optional outputs from symmetrize.py

--- diffuse_oldDetector/plot_diffuse.py
+++ diffuse_oldDetector/plot_diffuse.py
@@ -10,23 +10,29 @@ Instructions
    (set different_scans flag accordingly)
 1. Change address of output file you want to load from step 0 
 2. Change delay stage values and make sure time zero is correct
-3. 
+3. Decide if you want to oversaturate the image and what you want the maximum to be
+   (Do this with the clim_flag and clim_value)
+4. Which run in the run list do you want as the reference? (reference_num)
 '''
 
 ''' Values to change for each run'''
-load_address = r'C:\Users\tchase56\Documents\UED\Ni\data\20160628\short\\'
+load_address = r'C:\Users\tchase56\Documents\UED\Gold_Diffuse_03212016\20160512\short_scan\\'
 # Delay stage settings
-#delayStage_start, delayStage_end, delayStage_step  = [64.3, 65.5, 0.2] 
-delayStage_start, delayStage_end, delayStage_step  = [63.7, 64.30, 0.1]
-
-time_zero = 63.86
+delayStage_start, delayStage_end, delayStage_step  = [41.4, 45.0, 0.6]
+time_zero = 42.3
 # Flags
 clim_flag = 1     # Do you want to saturate large values on colorbar when plotting?
 clim_value = 10     # maximum value on colorbar when saturating large intensities
 # Which image to use for reference?
 reference_num = 0
-# Is this the output of align_average or the output of align_different_scans
-different_scans = 1
+# Flag for different file outputs
+# (0, alignment output)
+# (1, averaging runs output)
+# (2, centered rotated)
+# (3, symmetrized)
+# (4, centered rotated expanded)
+# (5, centered rotated expanded symmetrized)
+different_scans = 5
 
 
 
@@ -52,8 +58,17 @@ if different_scans == 0:
 elif different_scans == 1:
     pumped = np.load(load_address + 'averaged_runs.npy')
     reference = pumped[reference_num,:,:]
-else:
+elif different_scans == 2:
+    pumped = np.load(load_address + 'centered_rotated.npy')
+    reference = pumped[reference_num,:,:]
+elif different_scans == 3:
     pumped = np.load(load_address + 'symmetrized.npy')
+    reference = pumped[reference_num,:,:]
+elif different_scans == 4:
+    pumped = np.load(load_address + 'expanded.npy')
+    reference = pumped[reference_num,:,:]
+else:
+    pumped = np.load(load_address + 'expanded_symmetrized.npy')
     reference = pumped[reference_num,:,:]
 
 # Plot diffuse scattering
